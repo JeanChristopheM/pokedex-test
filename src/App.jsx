7f2e8a665cec5ae998d6f84b5386656d16915bba
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2022-01-18 16:51:03 +0100

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useEffect, useState, useRef } from "react";
 import { getData } from "./logic";
 
 const App = () => {
+  //! Declaring my state variables
   const [pokemons, setPokemons] = useState([]);
   const [nextPage, setNextPage] = useState(null);
   const [prevPage, setPrevPage] = useState(null);
   const [isFetching, setIsFetching] = useState(false);
 
+  //! Putting a ref on my main div
   const divRef = useRef(null);
 
+  //! Gets called once on app launch by the useEffect below
   const initialLoad = async () => {
     setIsFetching(true);
     const data = await getData("https://pokeapi.co/api/v2/pokemon");
@@ -17,29 +20,11 @@ const App = () => {
     setPrevPage(data.previous);
   };
 
-  const fetchMore = async () => {
-    setIsFetching(true);
-    const data = await getData(nextPage);
-    setPokemons((prev) => [...prev, ...data.results]);
-    setNextPage(data.next);
-    setPrevPage(data.previous);
-  };
-
-  const onBottomHit = () => {
-    if (isFetching) return;
-    fetchMore();
-  };
-
-  const handleScroll = () => {
-    if (!divRef.current) {
-      return false;
-    }
-    if (divRef.current.getBoundingClientRect().bottom <= window.innerHeight)
-      onBottomHit();
-  };
-
+  //! Only executed on app mount (once)
   useEffect(() => {
+    //! INITIAL FETCH
     initialLoad();
+    //! ADDING AN EVENT LISTENER
     window.addEventListener("scroll", handleScroll, {
       passive: true,
     });
@@ -48,11 +33,37 @@ const App = () => {
     };
   }, []);
 
+  //! Gets called either on btn Click or on scroll down at bottom of page
+  const fetchMore = async () => {
+    setIsFetching(true);
+    const data = await getData(nextPage);
+    setPokemons((prev) => [...prev, ...data.results]);
+    setNextPage(data.next);
+    setPrevPage(data.previous);
+  };
+
+  //! Gets called once the nextPage state variable changes
   useEffect(() => {
     console.log("ready");
     setIsFetching(false);
   }, [nextPage]);
 
+  //! Gets called when we hit the bottom of the page
+  const onBottomHit = () => {
+    if (isFetching) return;
+    fetchMore();
+  };
+
+  //! Gets called when the user scrolls
+  const handleScroll = () => {
+    if (!divRef.current) {
+      return false;
+    }
+    if (divRef.current.getBoundingClientRect().bottom <= window.innerHeight)
+      onBottomHit();
+  };
+
+  //! Return statement
   return (
     <>
       {pokemons.length == 0 ? (
@@ -79,6 +90,7 @@ const App = () => {
               </div>
             );
           })}
+          {/* // ! Floating div in the middle of the page */}
           <div
             style={{
               position: "fixed",
